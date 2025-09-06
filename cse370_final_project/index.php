ec69fc9edbf1025390b2eea100d623a538213c55
MD. Meharab Hossain Alvy <140847025+MHAlvy@users.noreply.github.com>
2025-09-06 20:23:54 +0600

    Delete cse370_final_project directory

--- cse370_final_project/index.php
+++
@@ -1,9 +0,0 @@
-<?php include 'header.php'; ?>
-
-<div class="card" style="text-align: center;">
-    <h2>Welcome to CampusSync Hub!</h2>
-    <p>Your central place for reporting lost items, finding found items, and managing event registrations.</p>
-    <p>Please log in or sign up to continue.</p>
-</div>
-
-<?php include 'footer.php'; ?>
