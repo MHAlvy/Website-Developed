51798ccc42ef3827590cedbc2bd2e5276cd7a179
MD. Meharab Hossain Alvy <140847025+MHAlvy@users.noreply.github.com>
2025-09-06 23:46:01 +0600

    Delete cse370_final_project directory

--- cse370_final_project/footer.php
+++
@@ -1,7 +0,0 @@
-</div> <!-- End container -->
-
-<!-- Bootstrap JS Bundle Includes Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-
-</body>
-</html>
